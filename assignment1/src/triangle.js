10409088fcb8f3d0289b0202d8e6972d4bdb5665
Perry <plin32@ucsc.edu>
2024-04-14 14:57:35 -0700

    Added new function that allows for custom placements of triangles with color

--- assignment1/src/triangle.js
+++ assignment1/src/triangle.js
@@ -23,6 +23,32 @@ class Triangle {
     }
 }
 
+function colorTriangle(vertices, rgbArray) {
+    
+    var n = 3;
+    
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    // Bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // Write date into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+ 
+    // Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // Enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    gl.uniform4f(u_FragColor, rgbArray[0], rgbArray[1], rgbArray[2], rgbArray[3]);
+
+    // Draw the rectangle
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
 
 function drawTriangle(vertices) {
     /*
